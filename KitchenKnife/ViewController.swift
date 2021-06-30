6eb83eb8ba1e0ef64bdfb6faee2955853d4eede6
Georgius Yoga Dewantama <yogadewantama@gmail.com>
2021-06-30 14:43:05 +0700

    Initial Commit

---
+++ KitchenKnife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KitchenKnife
+//
+//  Created by Georgius Yoga Dewantama on 30/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
